94342745bd91b409d33e730af5e3198ea97b774d
IslamWalid <islamwalid83@gmail.com>
2022-10-13 16:12:29 +0200

    Refactor test script

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -3,45 +3,42 @@
 go run main.go &
 sleep 0.3
 
+failure() {
+    echo $1
+    [[ $(mount | grep mnt) ]] && fusermount -zu ./mnt
+    rmdir mnt
+    exit 1
+}
+
 String=$(cat ./mnt/String)
 if [[ $String != 'str' ]]; then
-    echo 'TEST FAILED: file "String" does not match struct value'
-    fusermount -zu ./mnt
-    exit 1
+    failure 'TEST FAILED: file "String" does not match struct value'
 fi
 
 Int=$(cat ./mnt/Int)
 if [[ $Int != '18' ]]; then
-    echo 'TEST FAILED: file "Int" does not match struct value'
-    fusermount -zu ./mnt
-    exit 1
+    failure 'TEST FAILED: file "Int" does not match struct value'
 fi
 
 Bool=$(cat ./mnt/Bool)
-if [[ $Bool != 'true' ]]; then
-    echo 'TEST FAILED: file "Bool" does not match struct value'
-    fusermount -zu ./mnt
-    exit 1
+if [[ $Bool == 'true' ]]; then
+    failure 'TEST FAILED: file "Bool" does not match struct value'
 fi
 
 find ./mnt/SubStructure >> /dev/null
 if [[ $? != 0 ]]; then
-    echo 'TEST FAILED: dir "SubStructure" does not exist'
+    failure 'TEST FAILED: dir "SubStructure" does not exist'
 fi
 
 Float=$(cat ./mnt/SubStructure/Float)
 if [[ $Float != 1.3 ]]; then
-    echo 'TEST FAILED: file "Float" does not match struct value'
-    fusermount -zu ./mnt
-    exit 1
+    failure 'TEST FAILED: file "Float" does not match struct value'
 fi
 
 sleep 1
 String=$(cat ./mnt/String)
 if [[ $String != 'new string' ]]; then
-    echo 'TEST FAILED: file "String" was not modified'
-    fusermount -zu ./mnt
-    exit 1
+    failure 'TEST FAILED: file "String" was not modified'
 fi
 
 fusermount -zu ./mnt
